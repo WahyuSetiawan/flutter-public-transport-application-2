c2f9b1e7c796f716ac4332a0e57219663ef29150
wahyu setiawan <wahyu.creator911@gmail.com>
2019-06-19 16:47:03 +0700

    add model

---
+++ lib/model/ticket.dart
@@ -0,0 +1,31 @@
+class Ticket {
+  final String from;
+  final String dateFrom;
+
+  final String to;
+  final String dateTo;
+
+  final int estimationTime;
+
+  final String passenger;
+  final String date;
+  final String bonus;
+  final int bus_number;
+  final String seat;
+  final int tickets;
+  final String arrival;
+
+  Ticket(
+      {this.from,
+      this.dateFrom,
+      this.to,
+      this.dateTo,
+      this.estimationTime,
+      this.passenger,
+      this.date,
+      this.bonus,
+      this.bus_number,
+      this.seat,
+      this.tickets,
+      this.arrival});
+}
